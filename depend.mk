a5c9405baf7a85643cb102ac8b5dfe9099c797c4
chuck <charlesrymal@gmail.com>
2015-02-11 11:17:15 -0800

    change dep

--- depend.mk
+++ depend.mk
@@ -1,16 +1,5 @@
 
 
-dependclean:
-	@rm -rf $(build_dir)/depends/*
-
-$(__dep_files): $(build_dir)/depends/%.cpp.d: $(src_dir)/%.cpp
-	@bash -c "echo -e \"$(COLOR_BLUE)build deps $@$(COLOR_RESET)\""
-	@mkdir -p $(dir $@)
-	@rm -f $@
-	@$(CC) -c $(CARGS) -MM $^ -MF $@ -MT $(build_dir)/objects/$*.cpp.o
-	@#cat $@
-
--include $(dep_files)
 
 
 
