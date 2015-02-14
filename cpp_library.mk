5d9e7a3c2ce19040f9fbda027f2217a31b339e1f
chuck <charlesrymal@gmail.com>
2015-02-14 11:33:58 -0800

    consolidate a little

--- cpp_library.mk
+++ cpp_library.mk
@@ -14,6 +14,29 @@ dep_files     = $(patsubst $(src_dir)/%.cpp,    $(build_dir)/depends/%.cpp.d,   
 
 pch_files     = $(patsubst $(inc_dir)/%.hpp,    $(inc_dir)/%.hpp.gch,             $(inc_files))
 
+GCH = g++ -c -x c++-header
+
+$(obj): $(build_dir)/objects/%.cpp.o: $(src_dir)/%.cpp
+	@bash -c "echo -e \"$(COLOR_BLUE)build $@$(COLOR_RESET)\""
+	@mkdir -p $(dir $@)
+	@mkdir -p $(dir $(build_dir)/depends/$*.cpp.d)
+	@$(MAKEDEPEND)
+	@$(CC) -c $(CARGS) -o $@ $<
+
+
+$(obj_processed): $(build_dir)/objects/%.cpp.o: $(build_dir)/processed/src/%.cpp
+	@bash -c "echo -e \"$(COLOR_BLUE)build $@$(COLOR_RESET)\""
+	@mkdir -p $(dir $@)
+	@mkdir -p $(dir $(build_dir)/depends/$*.cpp.d)
+	@$(MAKEDEPEND)
+	@$(CC) -c $(CARGS) -o $@ $<
+
+
+$(pch_files): $(inc_dir)/%.hpp.gch: $(inc_dir)/%.hpp
+	@bash -c "echo -e \"$(COLOR_BLUE)pch $@$(COLOR_RESET)\""
+	@mkdir -p $(dir $@)
+	@$(GCH) $(CARGS) -o $@ $<
+
 
 
 
