9ea49b5077a0f5917a9243422c00465863af2292
crymal <charles.rymal@nortek.com>
2017-02-24 16:25:09 -0800

    create header processing

---
+++ templates/CHeader.hpp
@@ -0,0 +1,12 @@
+{{ include_block_open }}
+
+{% block includes %}{% endblock %}
+
+{{ namespace_open }}
+
+{% block body %}{% endblock %}
+
+{{ namespace_close }}
+
+{{ include_block_close }}
+
--- test/c_library/source/a.cpp
+++ test/c_library/source/a.cpp
@@ -1,3 +1,4 @@
 
 #include <testlib/a.hpp>
+#include <testlib/b.hpp>
 
